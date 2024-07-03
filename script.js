74004e8433b050e5e54af64c469bcdfc028c9908
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-03 20:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', ()=>{
+	const buttons = document.querySelectorAll('.btn');
+	const audioPlay = document.querySelector('.audio');
+	buttons.forEach((button)=>{
+		button.addEventListener('click',(e)=>{
+			let sourceData=e.target.id;
+			if(sourceData==='stop') stop();
+			else playAudio(sourceData);
+		})
+	})
+	function playAudio(sourceData){
+		audioPlay.src=sourceData;
+		audioPlay.play();
+	}
+	function stop(){
+		audioPlay.pause();
+	}
+})
